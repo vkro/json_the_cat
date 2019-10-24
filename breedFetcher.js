c10020e06eb786780f06190a7405702be157f472
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-24 22:26:01 +0000

    refactored to async function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,38 @@
 const request = require('request');
-const input = process.argv.splice(2);
+const args = process.argv.splice(2);
 
 
 // Use 'request' to fetch (Siberian) data from API endpoint
 // https://api.thecatapi.com/v1/breeds/search
 
-const breedFetcher = function () {
+const breedFetcher = function(error, description) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(description);
+  }
+}
+
+
+const fetchBreedDescription = function (input, callback) {
   const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + input;
 
   request(url, (error, response, body) => {
-    if (error) {
-      console.error(error);
-    } else if (body === '[]') {
-      console.log('Breed not found - try again with another search term.');
+    console.log('breedFetcher: Searching for cat breed...');
+    if (body === '[]') {
+      breedFetcher("Breed not found = try again with another search term.", null);
+    
+    } else if (error) {
+      breedFetcher(error);
+    
     } else {
       let data = JSON.parse(body);
-      let catObj = data[0]
-      console.log(catObj['description']);
+      let catDescription = data[0].description;
+      breedFetcher(null, catDescription);
     }
   })
 };
 
-breedFetcher();
+fetchBreedDescription(args, breedFetcher);
+
+module.exports = { fetchBreedDescription };
