9608ee8ca2f80ccf7286c4373c01c576ae3c8736
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-24 21:46:05 +0000

    handles empty search results and errors

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,15 +5,20 @@ const input = process.argv.splice(2);
 // Use 'request' to fetch (Siberian) data from API endpoint
 // https://api.thecatapi.com/v1/breeds/search
 
-const breedFetcher = function() {
+const breedFetcher = function () {
   const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + input;
-  request(url, (error, response, body) => {
-    let data = JSON.parse(body);
-    let catObj = data[0]
-    console.log(catObj['description']);
 
+  request(url, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else if (body === '[]') {
+      console.log('Breed not found - try again with another search term.');
+    } else {
+      let data = JSON.parse(body);
+      let catObj = data[0]
+      console.log(catObj['description']);
+    }
   })
 };
-
 
 breedFetcher();
