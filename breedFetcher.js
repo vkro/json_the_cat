5c8b6b531b21bc259cc3579d6bea016a9b32add1
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-24 21:00:36 +0000

    given API endpoint/URL, fetches & prints breed info

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,17 @@
 const request = require('request');
+
+
+// Use 'request' to fetch (Siberian) data from API endpoint
+// https://api.thecatapi.com/v1/breeds/search
+
+const breedFetcher = function(url, input) {
+  request(url, (error, response, body) => {
+    let catObj = JSON.parse(body);
+    console.log(typeof catObj);
+    console.log(catObj);
+  
+  })
+};
+
+
+breedFetcher('https://api.thecatapi.com/v1/breeds/search?q=sib');
