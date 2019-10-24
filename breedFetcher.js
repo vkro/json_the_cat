ddcc093653bc868811fe19318276a5ad29be6b27
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-24 21:20:16 +0000

    allow user to specify breed name using command-line args

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,19 @@
 const request = require('request');
+const input = process.argv.splice(2);
 
 
 // Use 'request' to fetch (Siberian) data from API endpoint
 // https://api.thecatapi.com/v1/breeds/search
 
-const breedFetcher = function(url, input) {
+const breedFetcher = function() {
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + input;
   request(url, (error, response, body) => {
-    let catObj = JSON.parse(body);
-    console.log(typeof catObj);
-    console.log(catObj);
-  
+    let data = JSON.parse(body);
+    let catObj = data[0]
+    console.log(catObj['description']);
+
   })
 };
 
 
-breedFetcher('https://api.thecatapi.com/v1/breeds/search?q=sib');
+breedFetcher();
